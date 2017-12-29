97023f20d8f98dc552fc100d3446cdaa8205f171
DEEPAK JANGIR <deepakjnitt@gmail.com>
2017-12-29 17:26:56 +0530

    adding filrd

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ app.get('/', function (req, res) {
   res.sendFile('index.html',{ root : __dirname});
 });
 
-app.get('/fb', function(req, res) {
+app.get('/facebook', function(req, res) {
 	res.sendFile('fb.png',{ root : __dirname});
+});
+
+app.get('/instagram', function(req, res) {
+	res.sendFile('instagram.png',{ root : __dirname});
+});
+
+app.get('/gmail', function(req, res) {
+	res.sendFile('gmail.png',{ root : __dirname});
+});
+
+app.get('/tyrebuddies', function(req, res) {
+	res.sendFile('tyrebuddies.png',{ root : __dirname});
 });
 
 //start a server on port 80 and log its start to our console
